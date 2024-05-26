8eaba6fc044e928bcafeab8eed36dfcaa8aa723d
arbnoraqorraj <115947900+arbnoraqorraj@users.noreply.github.com>
2024-05-26 16:09:19 +0200

    Update NavBar.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -68,6 +68,7 @@ const NavBar = () => {
                 <li><Link to="/women">Women</Link></li>
                 <li><Link to="/men">Men</Link></li>
                 <li><Link to="/kids">Kids</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
             </ul>
             <div className="nav-login-cart">
                 {token ? (
