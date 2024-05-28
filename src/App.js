e9da0886195f85d36a7a97d99f88823b869c4fd1
leootrimi <leootrim221@gmail.com>
2024-05-28 19:02:45 +0200

    hh

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter as Router, Routes, useLocation } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import AccountDetails from './components/Account/AccountDetails.jsx';
 import ContactForm from './components/Contact/ContactForm.jsx';
@@ -17,38 +17,14 @@ import WomenPage from "./pages/WomenPage.jsx";
 import Payment from "./pages/Payment.jsx";
 import ShoppingCart from "./pages/ShoppingCart.jsx"
 import OrdersPage from "./pages/OrdersPage.jsx"
+
 import ChangePass from './components/Account/ChangePass.jsx';
-
-const AppRoutes = () => {
-  const location = useLocation();
-
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/women" element={<WomenPage />} />
-      <Route path="/men" element={<MenPage />} />
-      <Route path="/productL" exact element={<ProductListing />} />
-      <Route path="/productL/:productId" exact element={<ProductDetail />} />
-      <Route path="/kids" element={<ShopCategory category="kids" />} />
-      <Route path="/contact" element={<ContactForm />} />
-      <Route path='/Checkout' element={<Payment />} />
-      <Route path="/product" element={<Product />}>
-        <Route path=":productId" element={<Product />} />
-      </Route>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<SignupForm />} />
-      <Route path="/account" element={<AccountDetails />} />
-      <Route path="/account/password" element={<ChangePass />} />
-    </Routes>
-  );
-};
 
 function App() {
   return (
     <Router>
       <div className="app-container">
         <NavBar />
-        <AppRoutes />
         <Routes>
           <Route path="/" element={<HomePage />} />   
           <Route path="/women" element={<WomenPage />} /> 
